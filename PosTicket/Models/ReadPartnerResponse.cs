9ed839a4c5dd3cd29c151169eaf47fbc39cdc120
Rachmat Aditiya <radit@arkana.co.id>
2019-12-19 09:25:32 +0700

    [ADD] sales person and customer model repository

---
+++ PosTicket/Models/ReadPartnerResponse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PosTicket.Repository.Interface;
+using PosTicket.Repository.WebService;
+
+namespace PosTicket.Models
+{
+    sealed class ReadPartnerResponse
+    {
+        public IPartnerRepository Repository { get; private set; }
+        public async Task<SalesPerson> GetSalesPersonAsync()
+        {
+            Repository = new PartnerRepository();
+            return await Repository.GetSalesPersonAsync();
+        }
+        public async Task<Customer> GetCustomerAsync()
+        {
+            Repository = new PartnerRepository();
+            return await Repository.GetCustomerAsync();
+        }
+    }
+}
---
+++ PosTicket/Repository/Interface/SalesPerson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using System.Text;
+
+namespace PosTicket.Repository.Interface
+{
+    public class SalesPerson
+    {
+        public List<SalesPersonData> result { get; set; }
+        public string version { get; set; }
+    }
+    public class SalesPersonData
+    {
+        public ErrorMessage error { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
